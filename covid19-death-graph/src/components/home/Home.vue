4229490495513ca8480bcccb187b69f6a1a610e6
euberdeveloper <euberdeveloper>
2020-03-28 02:29:13 +0100

    improvement: starting graph

--- covid19-death-graph/src/components/home/Home.vue
+++ covid19-death-graph/src/components/home/Home.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="home">
-    home
+    <canvas id="chart" width="400" height="400"></canvas>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { Chart } from 'chart.js';
 
 @Component
 export default class Home extends Vue {
- 
+  private data = {
+
+  }
+
+  private options = {
+    
+  }
+
+  mounted() {
+    const ctx = (document as any).getElementById('chart').getContext('2d');
+    let myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: [1,2,3,4,5,6,7,8,9,0],
+      options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+    });
+    }
 }
 </script>
 
@@ -17,5 +41,10 @@ export default class Home extends Vue {
 .home {
   height: 100%;
   width: 100%;
+  
+  canvas {
+    width: 400px !important;
+    height: 400px !important;
+  }
 }
 </style>

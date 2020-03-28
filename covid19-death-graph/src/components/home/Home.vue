bf0d06123084226cad09e993e01ceedefe2e9b61
euberdeveloper <euberdeveloper>
2020-03-28 02:07:44 +0100

    improvement: started vue project

---
+++ covid19-death-graph/src/components/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    home
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue {
+ 
+}
+</script>
+
+<style lang="scss">
+.home {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ covid19-death-graph/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home-view">
+    <home />  
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import Home from '@/components/home/Home.vue';
+
+@Component({
+  components: {
+    Home
+  }
+})
+export default class HomeView extends Vue {
+}
+</script>
+
+<style lang="scss">
+.home-view {
+  height: 100%;
+  width: 100%;
+}
+</style>
